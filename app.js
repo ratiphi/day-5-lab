f876203bbfdf58f152cdd184f72526143bee5dea
Tim Turner <timmyturner@me.com>
2017-05-12 17:29:57 -0700

    finished question 4

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ sum();
 testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for
-//your work on the next question!
+// your work on the next question!
 
 /////////////////////////////////////
 /* Problem 2
@@ -41,7 +41,7 @@ function multiply(a,b){ //eslint-disable-line
 testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for
-//your work on the next question!
+// your work on the next question!
 
 /////////////////////////////////////
 /* Problem 3
@@ -69,25 +69,32 @@ function sumAndMultiply(a,b,c){ //eslint-disable-line
 // Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4,7,5);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for
+// your work on the next question!
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the
+sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this.
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
+IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created.
+You're going to have to be resourceful to figure out how to do this.
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and
+see if the test passes.*/
 
 // Write your code here
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
-
+  var varSum1 = sum(testArray[0],testArray[1])[0];
+  var varSum2 = sum(varSum1,testArray[2])[0];
+  var varMsg = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + varSum2 + ' is their sum.';
+  return [varSum2, varMsg];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
