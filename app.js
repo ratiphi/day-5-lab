4b0d25b9a8ba6252471cc60f85111298f151bdb3
Tim Turner <timmyturner@me.com>
2017-05-12 17:10:43 -0700

    finished question 3

--- app.js
+++ app.js
@@ -19,13 +19,16 @@ sum();
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for
+//your work on the next question!
 
 /////////////////////////////////////
 /* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers,
+and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 "The product of 5 and 9 is 45."
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and
+see if the test passes.*/
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
@@ -37,23 +40,34 @@ function multiply(a,b){ //eslint-disable-line
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for
+//your work on the next question!
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of
+those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this
+example and use the values that were input into the function:
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
+IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication,
+use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function
+and see if the test passes.*/
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var varSum1 = sum(a,b)[0];
+  var varSum2 = sum(varSum1,c)[0];
+  var varProduct1 = multiply(a,b)[0];
+  var varProduct2 = multiply(varProduct1,c)[0];
+  var varMsg1 = a + ' and ' + b + ' and ' + c + ' sum to ' + varSum2 + '.';
+  var varMsg2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + varProduct2 + '.';
+  return [varSum2, varProduct2, varMsg1, varMsg2];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
